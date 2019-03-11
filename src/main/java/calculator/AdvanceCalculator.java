a1968c8221a1ecb11b1224707795a91cbfe15a9d
KamilJava <kamillapka1@gmail.com>
2019-03-11 18:49:44 +0100

    add advance version

---
+++ src/main/java/calculator/AdvanceCalculator.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class AdvanceCalculator {
+
+	//Trainer version
+	private static final String NUMBERS_DELIMITER = ",";
+
+	static int add(String numbers) {
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		Collection<Integer> parsedNumbers = parseNumbers(numbers);
+		validateNonNegativeNumbers(parsedNumbers);
+		return parsedNumbers.stream().filter(n -> n <= 1000).reduce(0,
+			Integer::sum);
+	}
+
+	private static void validateNonNegativeNumbers(Collection<Integer>
+							       parsedNumbers) {
+		String negativesNumbersString = parsedNumbers.stream().filter
+			(n -> n < 0).map(String::valueOf).collect(Collectors
+			.joining(","));
+		if (!negativesNumbersString.isEmpty()) {
+			throw new IllegalArgumentException("negatives not " +
+				"allowed: " + negativesNumbersString);
+		}
+	}
+
+	private static Collection<Integer> parseNumbers(String numbers) {
+		String extractedNumbers = extractNumbersAfterDelimiter
+			(numbers).replace("\n", NUMBERS_DELIMITER);
+		return Stream.of(extractedNumbers.split(NUMBERS_DELIMITER))
+			.map(Integer::valueOf).collect(Collectors.toList());
+	}
+
+	private static String extractNumbersAfterDelimiter(String numbers) {
+		if (numbers.matches("^//.\n.*$")) {
+			String delimiter = String.valueOf(numbers.charAt(2));
+			String numbersAfterDelimiter = numbers.substring(4);
+			return numbersAfterDelimiter.replace(delimiter,
+				NUMBERS_DELIMITER);
+		}
+		return numbers;
+	}
+}
---
+++ src/test/java/calculator/AdvanceCalculatorTest.java
@@ -0,0 +1,56 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+class AdvanceCalculatorTest {
+
+	//Trainer version
+	@DisplayName("should calculate sum according to the specification")
+	@ParameterizedTest(name = "given numbers: \"{0}\", expected sum: {1}")
+	// @formatter:off
+	@CsvSource({
+		"'', 0",
+		"'1', 1",
+		"'2', 2",
+		"'1,2', 3",
+		"'1,2,3', 6",
+		"'1\n2,3', 6",
+		"'//;\n1;2', 3",
+		"'//%\n1%2', 3",
+		"'2,1001', 2",
+		"'//%\n1%2%1000%1001%1002', 1003",
+	})
+	// @formatter:on
+	void test0(String numbers, int expectedSum) throws Exception {
+		// when
+		int sum = AdvanceCalculator.add(numbers);
+
+		// then
+		assertThat(sum).isEqualTo(expectedSum);
+	}
+
+	@DisplayName("should throw exception when negative numbers are given")
+	@ParameterizedTest(name = "given numbers: \"{0}\", expected ex msg: "
+		+ "{1}")
+	// @formatter:off
+	@CsvSource({
+		"'1,4,-1', 'negatives not allowed: -1'",
+		"'1,-1,-2', 'negatives not allowed: -1,-2'"
+	})
+	// @formatter:on
+	void test1(String numbers, String expectedExMsg) throws Exception {
+		// when
+		Throwable thrownException = catchThrowable(() ->
+			AdvanceCalculator.add(numbers));
+
+		// then
+		assertThat(thrownException).isInstanceOf
+			(IllegalArgumentException.class).hasMessage
+			(expectedExMsg);
+	}
+}
