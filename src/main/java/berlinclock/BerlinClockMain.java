65dbb66c349ef65abb2bd553e4e095ff9fb659b4
KamilJava <kamillapka1@gmail.com>
2019-03-12 15:31:02 +0100

    change format time

--- src/main/java/berlinclock/BerlinClockMain.java
+++ src/main/java/berlinclock/BerlinClockMain.java
@@ -1,13 +1,15 @@
 package berlinclock;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class BerlinClockMain {
     public static void main(String[] args) throws InterruptedException {
         //Berlin clock on LocalTime Now
         while (true){
-        System.out.println(BerlinClock.getFullTime(LocalTime.now())+"    "+LocalTime.now());
-        Thread.sleep(1000);
+                System.out.println(BerlinClock.getFullTime(LocalTime.now())+ "    "+
+                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+                Thread.sleep(1000);
     }
     }
 }
